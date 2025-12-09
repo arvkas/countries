515ee141e016ca6e74597d166a16ccebbca29eb2
vikas <vikas.ar@tekzensys.com>
2025-12-09 18:31:30 +0530

    world

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
   return (
     <header className={`header-container ${isDark ? 'dark':''}`}>
       <div className="header-content">
-        <h2 className="title"><a href="/">Where in the world?</a></h2>
+        <h2 className="title"><a href="/">Where in the world ??</a></h2>
         <p className="theme-changer" onClick={() => {
           setIsDark(!isDark)
           localStorage.setItem('isDarkMode', !isDark)
